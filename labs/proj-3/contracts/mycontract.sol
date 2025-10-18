144eb2e9f36c3be6373533213d8035359daf1e65
tim-yiu <timyiutyty@gmail.com>
2025-10-18 14:35:50 +0800

    add proj 3

---
+++ labs/proj-3/contracts/mycontract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// DO NOT MODIFY BELOW THIS
+pragma solidity ^0.8.17;
+
+import "forge-std/console.sol";
+
+contract Splitwise {
+// DO NOT MODIFY ABOVE THIS
+
+// ADD YOUR CONTRACT CODE BELOW
+
+}
